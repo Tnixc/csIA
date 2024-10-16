f5dc9d624e199342393802271c41ab03a1878c32
Tnixc <tnixxc@gmail.com>
2024-10-15 20:39:28 -0400

    fix stuff

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-from flask import Flask
+from flask import Flask, session, abort, redirect, request, render_template
 from flask_sqlalchemy import SQLAlchemy
 import requests
-from flask import session, abort, redirect, request, render_template
 from google.oauth2 import id_token
 from google_auth_oauthlib.flow import Flow
-from pip._vendor import cachecontrol
-import google.auth.transport.requests
+from google.auth.transport import requests as google_auth_requests
 import os
 import pathlib
 from flask_wtf import FlaskForm
@@ -14,18 +12,18 @@ from werkzeug.utils import secure_filename
 from wtforms.validators import InputRequired
 import speech_recognition as sr
 import soundfile
+from datetime import datetime
 
 app = Flask(__name__)
-app.config ['SECRET_KEY'] = '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'
+app.config['SECRET_KEY'] = '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'  # Replace with a secure secret key
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
 app.config['UPLOAD_FOLDER'] = 'static/files'
 
 db = SQLAlchemy(app)
-os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1" # to allow http traffic for local dev
+
+os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"  # To allow HTTP traffic for local dev
 GOOGLE_CLIENT_ID = "888946891767-ddsprhbd0kgmt0l95bbfhheir6b1r33h.apps.googleusercontent.com"
 client_secrets_file = os.path.join(pathlib.Path(__file__).parent, "client_secret.json")
-
-
 
 flow = Flow.from_client_secrets_file(
     client_secrets_file=client_secrets_file,
@@ -33,16 +31,25 @@ flow = Flow.from_client_secrets_file(
     redirect_uri="http://localhost/callback"
 )
 
+class CallDatabase(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    filename = db.Column(db.String(50))
+    calltext = db.Column(db.String(500), nullable=False)
+    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    time = db.Column(db.String(500), nullable=False)
+
+class UploadFileForm(FlaskForm):
+    file = FileField("File", validators=[InputRequired()])
+    submit = SubmitField("Upload File")
 
 def login_is_required(function):
     def wrapper(*args, **kwargs):
         if "google_id" not in session:
             return abort(401)  
         else:
-            return function()
+            return function(*args, **kwargs)
     wrapper.__name__ = function.__name__
     return wrapper
-
 
 @app.route("/login")
 def login():
@@ -50,21 +57,18 @@ def login():
     session["state"] = state
     return redirect(authorization_url)
 
-
 @app.route("/callback")
 def callback():
     flow.fetch_token(authorization_response=request.url)
 
-    if not session["state"] == request.args["state"]:
+    if not session.get("state") == request.args.get("state"):
         abort(500)  # State does not match!
 
     credentials = flow.credentials
-    request_session = requests.session()
-    cached_session = cachecontrol.CacheControl(request_session)
-    token_request = google.auth.transport.requests.Request(session=cached_session)
+    token_request = google_auth_requests.Request()
 
     id_info = id_token.verify_oauth2_token(
-        id_token=credentials._id_token,
+        id_token=credentials.id_token,
         request=token_request,
         audience=GOOGLE_CLIENT_ID
     )
@@ -73,11 +77,9 @@ def callback():
     session["name"] = id_info.get("name")
     return redirect("/home")
 
-
 @app.route("/logout")
 def logout():
-    for key in list(session.keys()):
-        session.pop(key)
+    session.clear()
     return redirect("/")
 
 @app.route("/")
@@ -87,7 +89,7 @@ def loginpage():
 @app.route("/home")
 @login_is_required
 def home():
-    return render_template("base.html") #f"Hello {session['name']}! <br/> <a href='/logout'><button>Logout</button></a><a href='/home/report'><button>Report</button></a><a href='/home/translate'><button>Translate</button></a>"
+    return render_template("base.html")
 
 @app.route("/home/report")
 @login_is_required
@@ -98,17 +100,6 @@ def report():
 @login_is_required
 def manage():
     return render_template("base.html")
-
-class CallDatabase(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    filename = db.Column(db.String(50))
-    calltext = db.Column(db.String(500), nullable=False)
-    date = db.Column(db.String(100), nullable=False)
-    time = db.Column(db.String(500), nullable=False)
-
-class UploadFileForm(FlaskForm):
-    file = FileField("File", validators=[InputRequired()])
-    submit = SubmitField("Upload File")
 
 def stt_Cantonese(audio_file):
     data, samplerate = soundfile.read(audio_file)
@@ -129,12 +120,12 @@ def stt_Cantonese(audio_file):
 def translate():
     form = UploadFileForm()
     if form.validate_on_submit():
-        file = form.file.data # First grab the file
-        file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['UPLOAD_FOLDER'],secure_filename("audio_file.wav"))) # Then save the file
+        file = form.file.data
+        file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)), app.config['UPLOAD_FOLDER'], secure_filename("audio_file.wav")))
         
-    return render_template("translate.html", form = form)
-
+    return render_template("translate.html", form=form)
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(host='0.0.0.0', port=80, debug=True)
-
